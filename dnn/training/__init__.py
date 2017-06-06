ccdf43e41a8dd72fb61aee4cd563bdb630ec6020
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2017-06-05 23:13:32 -0700

    Deleted monitor module from __init__.py and added learning_curve module to __init__.py.

--- dnn/training/__init__.py
+++ dnn/training/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from .back_propagation import *
+from .learning_curve import *
 from .loss_function import *
 from .optimizer import *
 from .random_weight import *
--- dnn/utils/__init__.py
+++ dnn/utils/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
 
-from .debug_utils import *
 from .im2col import *
 from .nn_utils import *

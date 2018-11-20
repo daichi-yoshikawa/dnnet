32112b48dc6d2daf4232e81d555710f7267f9f00
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2018-11-19 22:52:13 -0800

    Implemented PoolingLayer.

--- dnn/layers/pooling.py
+++ dnn/layers/pooling.py
@@ -4,11 +4,16 @@
 import numpy as np
 
 from dnn.layers.layer import Layer
-from dnn.utils.conv_utils import im2col
+from dnn.utils.conv_utils import im2col, col2im
 
 class PoolingLayer(Layer):
-    def __init__(self, pool_shape):
-        self.pool_shape = pool_shape
+    def __init__(self, window_shape):
+        self.window_shape = window_shape
+        self.pad = (0, 0)
+        self.strides = (1, 1)
+
+    def set_dtype(self, dtype):
+        self.dtype = dtype
 
     def get_type(self):
         return 'pooling'
@@ -16,18 +21,79 @@ class PoolingLayer(Layer):
     def set_parent(self, parent):
         Layer.set_parent(self, parent)
 
+        self.__check_shape(self.input_shape)
+        self.__set_output_shape()
+
     def forward(self, x):
-        pass
+        self.__forward(x)
+        self.child.forward(self.fire)
 
     def backward(self, dy):
-        pass
+        self.__backward(dy)
+        self.parent.backward(self.backfire)
 
     def predict(self, x):
-        pass
+        self.__forward(x)
+        return self.child.predict(self.fire)
 
     def __forward(self, x):
-        pass
+        if len(x.shape) != 4:
+            msg = 'Pooling layer only supports 4-d array.\n'\
+                + '    input shape : %s' % str(x.shape)
+            raise RuntimeError(msg)
+
+        n_batches, _, _, _ = x.shape
+        n_channels, n_rows, n_cols = self.output_shape
+
+        window_shape = [1]
+        window_shape.extend(self.window_shape)
+        window_shape = tuple(window_shape)
+
+        self.x = im2col(x, window_shape, self.strides)
+        self.x = self.x.reshape(self.x.shape[0] * n_channels, -1)
+        self.fire = np.max(self.x, axis=1)
+        self.fire = self.fire.reshape(n_batches, n_rows, n_cols, n_channels).transpose(0, 3, 1, 2)
 
     def __backward(self, dy):
-        pass
+        indices = [np.arange(self.x.shape[0], dtype=int), np.argmax(self.x, axis=1)]
+        grad = np.zeros_like(self.x, dtype=self.x.dtype)
+        grad[indices] = 1
+        self.backfire = grad * dy.flatten().reshape(-1, 1)
 
+        n_batches, _, _, _ = self.fire.shape
+        n_channels, n_rows, n_cols = self.input_shape
+        input_shape = (n_batches, n_channels, n_rows, n_cols)
+        window_shape = [1]
+        window_shape.extend(self.window_shape)
+        window_shape = tuple(window_shape)
+
+        self.backfire = col2im(
+                self.backfire, input_shape, self.output_shape,
+                window_shape, self.pad, self.strides, aggregate=True)
+
+    def __check_shape(self, shape):
+        if not isinstance(shape, tuple):
+            msg = 'Invalid type of shape : ' + type(shape)
+            raise RuntimeError(msg)
+        elif len(shape) != 3:
+            msg = 'Invalid shape : ' + str(shape) + '\nShape must be (channels, rows, cols).'
+            raise RuntimeError(msg)
+
+    def __set_output_shape(self):
+        n_channels, n_rows_in, n_cols_in = self.input_shape
+        n_rows_window, n_cols_window = self.window_shape
+
+        rem_rows = (n_rows_in + 2*self.pad[0] - n_rows_window) % self.strides[0]
+        rem_cols = (n_cols_in + 2*self.pad[1] - n_cols_window) % self.strides[1]
+
+        if(rem_rows > 0) or (rem_cols > 0):
+            msg = 'Invalid combos of input shape, window size, pad, and stride.\n'\
+                + '    input shape : %s\n' % str(self.input_shape)\
+                + '    window size : %s\n' % str(self.window_shape)\
+                + '    pad, stride : %s, %s' % (str(self.pad), str(self.strides))
+            raise RuntimeError(msg)
+
+        n_rows_out = (n_rows_in + 2*self.pad[0] - n_rows_window) // self.strides[0] + 1
+        n_cols_out = (n_cols_in + 2*self.pad[1] - n_cols_window) // self.strides[1] + 1
+
+        self.output_shape = (n_channels, n_rows_out, n_cols_out)

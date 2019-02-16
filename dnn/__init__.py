c33aef4beba78c159f9cec9e45ee21b0902badd7
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2019-02-17 02:08:47 +0900

    Added extra info.

--- dnn/__init__.py
+++ dnn/__init__.py
@@ -1,1 +1,6 @@
+__copyright__ = 'Copyright (C) 2017 Daichi Yoshikawa'
 __version__ = '0.1.0'
+__license__ = 'BSD-3-Clause'
+__author__ = 'Daichi Yoshikawa'
+__author_email__ = 'daichi.yoshikawa@gmail.com'
+__url__ = 'https://github.com/daichi-yoshikawa/dnn'

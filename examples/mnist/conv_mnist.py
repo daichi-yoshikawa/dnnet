16a6e6b5ac12ff38c105def0868b2b2c91f29d5e
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-01-08 21:48:02 -0800

    Made sample neural network smaller to reduce computation cost, but still result in goo accuracy like 99.1~3% for test dataset. Also, enabled GPU.

--- examples/mnist/conv_mnist.py
+++ examples/mnist/conv_mnist.py
@@ -7,7 +7,11 @@ sys.path.append('../..')
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+
 import dnnet
+from dnnet.config import Config
+Config.enable_gpu()
+
 from dnnet.neuralnet import NeuralNetwork
 from dnnet.utils.nn_utils import scale_normalization
 
@@ -25,67 +29,77 @@ from dnnet.layers.pooling import PoolingLayer
 from data import get_mnist
 
 
+x, y = get_mnist('../../data')
+x = x[:10000]
+y = y[:10000]
+scale_normalization(x)
+x = x.reshape(-1, 1, 28, 28)
+
 dtype = np.float32
+force_cpu = {
+    'activation': True,
+    'affine': False,
+    'batch_norm': True,
+    'convolution': False,
+    'dropout': True,
+    'pooling': False,
+}
+
 model = NeuralNetwork(input_shape=(1, 28, 28), dtype=dtype)
 
 model.add(
     ConvolutionLayer(
         filter_shape=(32, 3, 3), pad=(0, 0), strides=(1, 1),
-        weight_initialization=He()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.relu))
+        weight_initialization=He(), force_cpu=force_cpu['convolution']))
+model.add(BatchNormLayer(force_cpu=force_cpu['batch_norm']))
+model.add(ActivationLayer(
+    activation=Activation.Type.relu, force_cpu=force_cpu['activation']))
+model.add(DropoutLayer(drop_ratio=0.25, force_cpu=force_cpu['dropout']))
 
 model.add(
     ConvolutionLayer(
         filter_shape=(32, 3, 3), pad=(0, 0), strides=(1, 1),
-        weight_initialization=He()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.relu))
-model.add(DropoutLayer(drop_ratio=0.25))
+        weight_initialization=He(), force_cpu=force_cpu['convolution']))
+model.add(BatchNormLayer(force_cpu=force_cpu['batch_norm']))
+model.add(ActivationLayer(
+    activation=Activation.Type.relu, force_cpu=force_cpu['activation']))
 
 model.add(
     ConvolutionLayer(
         filter_shape=(64, 3, 3), pad=(0, 0), strides=(1, 1),
-        weight_initialization=He()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.relu))
+        weight_initialization=He(), force_cpu=force_cpu['convolution']))
+model.add(BatchNormLayer(force_cpu=force_cpu['batch_norm']))
+model.add(ActivationLayer(
+    activation=Activation.Type.relu, force_cpu=force_cpu['activation']))
 
-model.add(
-    ConvolutionLayer(
-        filter_shape=(64, 3, 3), pad=(0, 0), strides=(1, 1),
-        weight_initialization=He()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.relu))
+model.add(AffineLayer(
+    output_shape=512, weight_initialization=He()))
+model.add(BatchNormLayer(force_cpu=force_cpu['batch_norm']))
+model.add(ActivationLayer(
+    activation=Activation.Type.relu, force_cpu=force_cpu['activation']))
+model.add(DropoutLayer(drop_ratio=0.5, force_cpu=force_cpu['dropout']))
 
-model.add(AffineLayer(output_shape=256, weight_initialization=He()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.relu))
-model.add(DropoutLayer(drop_ratio=0.5))
+model.add(AffineLayer(
+    output_shape=10, weight_initialization=DefaultInitialization()))
+model.add(BatchNormLayer(force_cpu=force_cpu['batch_norm']))
+model.add(ActivationLayer(
+    activation=Activation.Type.softmax, force_cpu=force_cpu['activation']))
 
-model.add(AffineLayer(output_shape=10, weight_initialization=DefaultInitialization()))
-model.add(BatchNormLayer())
-model.add(ActivationLayer(activation=Activation.Type.softmax))
 model.compile()
-
 config_str = model.get_config_str()
 print(config_str)
 
-data_dir = '../../data'
-x, y = get_mnist(data_dir)
-scale_normalization(x)
-x = x.reshape(-1, 1, 28, 28)
-
-optimizer = AdaGrad(learning_rate=1e-3, weight_decay=1e-3, dtype=dtype)
+optimizer = AdaGrad(learning_rate=1e-2, weight_decay=1e-3, dtype=dtype)
 print('Learning Rate :', optimizer.learning_rate)
 
 lc = model.fit(
-    x=x, y=y, epochs=5, batch_size=100, optimizer=optimizer,
+    x=x, y=y, epochs=1, batch_size=128, optimizer=optimizer,
     loss_function=MultinomialCrossEntropy(),
     learning_curve=True, shuffle=True, shuffle_per_epoch=True,
     test_data_ratio=0.142857, # Use 60,000 for training and 10,000 for test.
     train_data_ratio_for_eval=0.01)
 lc.plot(figsize=(8,10), fontsize=12)
-model.save(path='output', name='mnist_conv_net.dat')
+model.save(path=None, name='mnist_conv_net.dat')
 
 #model.visualize_filters(index=0, n_rows=4, n_cols=8, filter_shape=(3, 3), figsize=(8, 8))
 #model.visualize_filters(index=0, shape=None, figsize=(8, 8))
